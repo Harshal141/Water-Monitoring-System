41cfa5d140860aa161cdcef36e6212546eca8068
Harshal141 <harshalmukundapatil@gmail.com>
2023-02-13 15:57:49 +0530

    login user using jwt token

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(express.json({}));
 app.use(express.urlencoded({ extended: false }));
 app.use('/public', express.static('public'));
 
-app.get('/', (req, res) => res.render('index.ejs',{error:''}));
+app.get('/home/:name', (req, res) => {
+    let name = req.params.name;
+    res.render('index.ejs',{error:'',name});
+})
 
 // Routes
 app.use('/',require('./routes/login'));
