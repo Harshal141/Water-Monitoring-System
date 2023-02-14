e6641173cdc071867613115f29ae35106bc7661f
Harshal141 <harshalmukundapatil@gmail.com>
2023-02-14 11:39:55 +0530

    auth middleware to verify session token

---
+++ middleware/notauth.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req,res,next) {
+    // get token from header
+    const token = req.header['x-auth-token'];
+    
+    if(token){
+        return res.redirect('/');
+    }
+    next();
+}
