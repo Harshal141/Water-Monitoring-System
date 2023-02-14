9bf811c6d1ae2efdde1a6775e69c91942b90ee85
Harshal141 <harshalmukundapatil@gmail.com>
2023-02-14 12:11:47 +0530

    accessing map rendering parameters backend

--- routes/map.js
+++ routes/map.js
@@ -7,4 +7,73 @@ const Data = require('../models/Data');
 const auth = require('../middleware/auth');
 const notauth = require('../middleware/notauth');
 
+// @route  GET map/place
+// @desc   rendering map for current location
+// @access private
+router.get('/map/:place',auth,async (req,res)=>{
+
+    let loc = req.params.place;
+    try{
+        
+        // check if the location exists
+        let data = await Data.findOne({"name":loc});
+        if(!data){
+            return res.render('index.ejs',{error: 'No data found'});
+        }
+
+        // Values required by map.ejs to render
+        let layer  = await Data.find({},{"_id":0,"__v":0,"data":0});
+        let layers = [],name = [],marker = [];
+        let flow, ph, temp, turbidity;
+
+        // storing values to var for graph view
+        await data.data.forEach((item)=>{
+            flow = JSON.parse(item.flow);
+            ph = JSON.parse(item.ph);
+            temp = JSON.parse(item.temp);
+            turbidity = JSON.parse(item.turbidity);
+        })
+        let graph = [flow,ph,temp,turbidity]
+
+        // acessing map markers and layers and storing in arrays
+        await layer.forEach((item)=>{
+            layers.push(item.layer);
+            name.push(item.name);
+            marker.push(item.marker);
+        })
+
+        // converting arrays to stings to send as parameters
+        layers = JSON.stringify(layers);
+        name = JSON.stringify(name);
+        graph = JSON.stringify(graph);
+        marker = JSON.stringify(marker);
+        console.log(marker)
+
+        // rendering map.ejs with all the vaues
+        res.render('map.ejs',{data,graph,layers,name,marker});
+
+        // Code to Enter value to database shortcut
+
+        // let user = new Data({ // create new user instance
+        //     name:"Jambhulwadi Lake",
+        //     data : [
+        //         {
+        //             flow: "[7,7,7,7,7,7,3]",
+        //             ph: "[7,7,7,7,7,7,3]",
+        //             temp: "[7,7,7,7,7,7,3]",
+        //             turbidity: "[7,7,7,7,7,7,3]"
+        //         }
+        //     ],
+        //     layer: "https://api.maptiler.com/data/26207568-dcd9-4685-8dfc-20f9ce448bde/features.json?key=nHu6zcWr7zTF7fGWAQNO"
+        // });
+
+        // await user.save(); // save user to database
+        // res.send(user);
+    
+    }catch(err){
+        console.error(err.message);
+        res.status(500).render('index.ejs', {error: 'Server Error'});
+    }
+})
+
 module.exports=router
